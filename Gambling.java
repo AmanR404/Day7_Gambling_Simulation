f6db93814e030d32c43f4e8ddf5b809983c5aa42
Aman <raiaman21@gmail.com>
2022-07-20 16:35:03 +0530

    UC4 Solved

--- Gambling.java
+++ Gambling.java
@@ -5,6 +5,8 @@ class game{
     int stake = 100;
     int rnnum;
     int counter;
+    int Lose;
+    int Won;
 
     public void Game(int min, int max){
         int range = (max - min) + 1;     
@@ -12,22 +14,28 @@ class game{
      }
 
     void ingame(){
+      for (int j = 0; j<=20;j++){
         for (int i = 0; stake != 0 || stake <= 101 || stake != 50; i++ ){
             Game(0, 1);
             if(rnnum == 0){
                 System.out.println("You Lose");
                 stake--;
                 counter++;
+                Lose++;
             }
             else  if (rnnum == 1){
                 System.out.println("You won");
                 stake++;
                 counter++;
+                Won++;
             }
-        }System.out.println("");
-        System.out.println("Total times game played : " + counter);
-        System.out.println("Your stake left : " + stake);
+        }
     }
+        System.out.println("In 20 days u won " + Won + " amount");
+        System.out.println("In 20 days u lose " + Lose + " amount");
+        System.out.println("");
+        System.out.println("Total times game played : " + counter);
+      }
 }
 
 public class Gambling{
